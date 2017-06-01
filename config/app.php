0d22e622e179a5e467bfb3e3ce64f26580481029
Master Weez <wizqydy@gmail.com>
2017-06-01 14:52:33 +0300

    Add Entrust and dompdf packages

--- config/app.php
+++ config/app.php
@@ -165,6 +165,8 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
+        Zizaco\Entrust\EntrustServiceProvider::class,
 
         /*
          * Application Service Providers...
